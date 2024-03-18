ed2a19e7b73f736fae2c2a031856bfcf9d4ff671
LexaFrontDev <156980843+LexaFrontDev@users.noreply.github.com>
2024-03-18 10:39:34 +0600

    APP-todo
    APP-todo

---
+++ js/main.js
@@ -0,0 +1,136 @@
+const form = document.querySelector('#form');
+const taskInput = document.querySelector('#taskInput');
+const tasksList = document.querySelector('#tasksList');
+const emptyList = document.querySelector('#emptyList')
+
+let tasks = [];
+
+if (localStorage.getItem('tasks')) {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+    tasks.forEach((task) => renderTask(task))
+}
+
+
+
+
+checkEmptyList();
+
+
+
+form.addEventListener('submit', addTask)
+tasksList.addEventListener('click', deleteTask);
+tasksList.addEventListener('click', doneTask)
+
+
+
+
+//функций
+function addTask(event) {
+    //Убираем дефлтные значение
+
+    event.preventDefault();
+
+   
+
+    const taskText = taskInput.value
+    
+    const newTask = {
+        id: Date.now(),
+        text: taskText,
+        done: false, 
+    };
+
+
+    tasks.push(newTask);
+
+
+      renderTask(newTask)
+   
+      taskInput.value = ''
+      taskInput.focus();
+
+      checkEmptyList();
+
+      saveTolocalStorage();
+}
+
+
+function deleteTask (event) {
+    if (event.target.dataset.action !== 'delete') return;
+    const parenNode = event.target.closest('li')
+    
+
+    const id = Number(parenNode.id); 
+
+    //const index = tasks.findIndex((task) => task.id === id);
+    //tasks.splice(index, 1)
+
+    tasks = tasks.filter((task) => task.id !== id)
+    saveTolocalStorage();
+
+    
+    parenNode.remove()
+}
+
+
+
+
+function doneTask (event) {
+    if (event.target.dataset.action !== 'done') return;
+    
+    const parenNode = event.target.closest('li');
+    const id = Number(parenNode.id);
+    const task = tasks.find((task) => task.id === id) 
+    task.done = !task.done
+
+    saveTolocalStorage();
+
+    const taskTitle =parenNode.querySelector('.task-title');
+    taskTitle.classList.toggle('task-title--done');
+
+    checkEmptyList()
+}
+
+
+function checkEmptyList () {
+    if (tasks.length === 0) {
+        const emptyListHtml = `
+        <li id="emptyList" class="list-group-item empty-list">
+              <img src="./img/leaf.svg" alt="Empty" width="48" class="mt-3">
+        <div class="empty-list__title">Список дел пуст</div>
+        </li>
+`
+
+        tasksList.insertAdjacentHTML('afterbegin', emptyListHtml)
+    }
+
+    if (tasks.length > 0) {
+        const emptyListEl = document.querySelector('#emptyList')
+        emptyListEl ?emptyListEl.remove() : null;
+    }
+}
+
+function saveTolocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+function renderTask(task) {
+    const cssClass = task.done ? "task-title task-title--done" : "task-title";
+
+   
+    const taskHtml = `
+    <li id="${task.id}" class="list-group-item d-flex justify-content-between task-item">
+    <span class="${cssClass}">${task.text}</span>
+    <div class="task-item__buttons">
+        <button type="button" data-action="done" class="btn-action">
+            <img src="./img/tick.svg" alt="Done" width="18" height="18">
+        </button>
+        <button type="button" data-action="delete" class="btn-action">
+            <img src="./img/cross.svg" alt="Done" width="18" height="18">
+        </button>
+    </div>
+</li>`
+
+    
+      tasksList.insertAdjacentHTML('beforeend', taskHtml);
+}
